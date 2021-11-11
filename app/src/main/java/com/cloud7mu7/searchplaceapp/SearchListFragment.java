7b9a852cb7c729679cc0f2c4389e11d4658db9e2
cloudmu7 <cloudmu7777@naver.com>
2021-11-11 14:40:19 +0900

    1

--- app/src/main/java/com/cloud7mu7/searchplaceapp/SearchListFragment.java
+++ app/src/main/java/com/cloud7mu7/searchplaceapp/SearchListFragment.java
@@ -8,11 +8,21 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SearchListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_search_list, container, false);
+    }
+
+    RecyclerView recyclerView;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.recyclerview);
     }
 }
